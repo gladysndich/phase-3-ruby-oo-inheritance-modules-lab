61e3a7f047b11016cb339ff46cc8aab210b48228
gladysndich <itsgladysbrian@gmail.com>
2022-08-22 12:20:23 +0300

    Completed, tests passing

--- lib/song.rb
+++ lib/song.rb
@@ -1,29 +1,20 @@
 require 'pry'
+require_relative '../lib/concerns/memorable'
+require_relative './concerns/findable'
 
 class Song
   attr_accessor :name
   attr_reader :artist
 
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  extend Findable::ClassMethods
+  include Paramable::InstanceMethods
+
   @@songs = []
-
-  def initialize
-    @@songs << self
-  end
-
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
 
   def self.all
     @@songs
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
   end
 
   def artist=(artist)
